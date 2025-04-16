c590bd7141c6f2dc4703b86e775caa76093fbc7a
Thiago Rasquino <thiago.programador.net@gmail.com>
2025-04-16 09:54:17 -0300

    first commit

---
+++ src/main/java/br/com/projects/thiago/my_own_service_discovery/MyOwnServiceDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package br.com.projects.thiago.my_own_service_discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOwnServiceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyOwnServiceDiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projects/thiago/my_own_service_discovery/MyOwnServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projects.thiago.my_own_service_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyOwnServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
